8a1c4b771c5ed3e8542165024ca8df0fd7e4260a
Rithvik Vanteru <rithvik@Rithviks-MacBook-Pro.local>
2022-09-26 01:41:33 -0400

    created control panel again

--- src/Profile.java
+++ src/Profile.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Profile {
+import javax.swing.*;
+
+public class Profile {
+    private JPanel Main_panel;
+    private JButton create_btn;
+    private JButton Read_btn;
+    private JButton Update_btn;
+    private JButton Delete_btn;
+    private JLabel name1;
+    private JLabel age1;
+    private JLabel gender1;
+    private JPanel control_panel;
+    private JPanel create_panel;
 }
